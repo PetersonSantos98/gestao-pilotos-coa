63a1ce5379937136efc3b743e2d6fbb42f4a7ef0
PetersonSantos98 <peterson_fsantos@hotmail.com>
2026-05-03 08:52:53 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from pages import home, frotas, editar, vencimentos, componentes
+
+st.set_page_config(page_title="Gestão COA", layout="centered")
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+
+def go(page):
+    st.session_state.page = page
+    st.rerun()
+
+st.markdown("## 🚜 Gestão de Pilotos - COA")
+
+# ROTEAMENTO
+p = st.session_state.page
+if p == "home": home.render(go)
+elif p == "frotas": frotas.render(go)
+elif p == "editar": editar.render(go)
+elif p == "vencimentos": vencimentos.render(go)
+# Para as páginas de Antenas, Monitores e NAVs, criamos um renderizador genérico
+elif p in ["antenas", "monitores", "navs"]: 
+    import pages.componentes as componentes
+    componentes.render(go, p)
