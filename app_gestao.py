cb687ce32bd049867306aa019b545d301fbff68f
PetersonSantos98 <peterson_fsantos@hotmail.com>
2026-05-03 09:18:32 -0300

    Update app_gestao.py

--- app_gestao.py
+++ app_gestao.py
@@ -1,27 +1,36 @@
 import streamlit as st
-from pages import home, frotas, editar, vencimentos
+from pages import home, frotas, editar, vencimentos, componentes
 
+# Configuração da página (Deve ser a primeira linha de comando Streamlit)
 st.set_page_config(page_title="Gestão COA", layout="centered")
 
+# Inicialização do estado da página
 if "page" not in st.session_state:
     st.session_state.page = "home"
 
+# Função de Navegação
 def go(page):
     st.session_state.page = page
     st.rerun()
 
-# HEADER
-st.markdown("## 🚜 Gestão de Pilotos")
+# Cabeçalho Fixo
+st.markdown("## 🚜 Gestão de Pilotos - COA")
 
-# ROUTER
-if st.session_state.page == "home":
+# --- ROTEADOR (ROUTER) ---
+p = st.session_state.page
+
+if p == "home":
     home.render(go)
 
-elif st.session_state.page == "frotas":
+elif p == "frotas":
     frotas.render(go)
 
-elif st.session_state.page == "editar":
+elif p == "editar":
     editar.render(go)
 
-elif st.session_state.page == "vencimentos":
+elif p == "vencimentos":
     vencimentos.render(go)
+
+# Aqui é onde os botões de componentes são processados
+elif p in ["antenas", "monitores", "navs"]:
+    componentes.render(go, p)
